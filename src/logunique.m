2ea9ce55e2c001d5266bea8668a7b3df8be3dcce
Mauro Torrez <mauro@qom>
2014-08-22 11:42:53 -0300

    logunique now supports 2d-vectors

--- src/logunique.m
+++ src/logunique.m
@@ -1,23 +1,30 @@
 function y = logunique ( x, precision )
-    
+
 %% logunique
 % filters out vector elements with very similar log value
 % according to precision parameter
-    
+
     if nargin < 2
         precision = 1e-10;
     end
-    
-    s = sort(x);
+
+    horizontal = 0;
+    if size(x,1)<size(x,2)
+        horizontal=1;
+        x=x';
+    end
+    N = size(x,1);
+
+    s = sortrows(x);
     l = log(s);
-    y = [ s(1) ];
-    for i=2:length(l)
-        if abs(l(i)-l(i-1)) > precision
-            if isrow(x)
-                y = [ y s(i) ];
-            else
-                y = [ y; s(i) ];
-            end
+    y = [ s(1,:) ];
+    for i=2:N
+        if sum(abs(l(i,:)-l(i-1,:)) > precision) > 0
+            y = [ y; s(i,:) ];
         end
+    end
+
+    if horizontal
+        y =y';
     end
 end
