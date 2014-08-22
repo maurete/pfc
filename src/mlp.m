c1d367a75d6481f0bfee5ac175c20631ebad0015
Mauro Torrez <mauro@qom>
2014-08-22 11:54:59 -0300

    mlp better testing

--- src/mlp.m
+++ src/mlp.m
@@ -1,8 +1,8 @@
 function mlp ( dataset, featset, balance, random_seeds )
-    
+
     if nargin < 3, balance = 0; end
     if nargin < 4, random_seeds = [303456; 456789; 5829]; end
-    
+
     % aux functions
     pick      = @(x,n) x(randsample(size(x,1),min(size(x,1),n)),:);
     shuffle   = @(x)   x(randsample(size(x,1),size(x,1)),:);
@@ -20,14 +20,14 @@ function mlp ( dataset, featset, balance, random_seeds )
              'extra+sequence', 'triplet+structure', 'triplet+sequence', ...
              'not-extra' };
     features = fidx{featset};
-    
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     fprintf('#\n> begin mlp\n#\n' );
 
     % keep record of this experiment for review
     S = struct();
-    S.random_seeds = random_seeds; 
+    S.random_seeds = random_seeds;
     S.featureset = featset;
     S.partitions = 5;
     S.hidden_sizes = [5:25];
@@ -35,7 +35,7 @@ function mlp ( dataset, featset, balance, random_seeds )
     S.begintime = clock;
     S.time = 0;
     S.numcv = 0;
-    
+
     S.data = struct();
     for i=1:length(S.random_seeds)
         [ S.data(i).train S.data(i).test] = load_data( dataset, S.random_seeds(i), false);
@@ -48,13 +48,13 @@ function mlp ( dataset, featset, balance, random_seeds )
         [S.data(i).cv_train_pseudo S.data(i).cv_test_pseudo] = ...
             stpart(S.random_seeds(i), S.data(i).train.pseudo, S.partitions);
     end
-    
+
     % file where to save tabulated train/test data
     tabfile = 'resultsv2.tsv'
     if ~exist( tabfile )
         fid = fopen( tabfile, 'a' );
         fprintf( fid, [ '#dsetup\tclass\tdataset\tfeatset\t' ...
-                        'classifier\tparam1\tparam2\tP\n' ] ); 
+                        'classifier\tparam1\tparam2\tP\n' ] );
         fclose(fid);
     end
     function writetab(fid,cls,dset,param,result)
@@ -62,7 +62,7 @@ function mlp ( dataset, featset, balance, random_seeds )
         fprintf(fid, '%s\t%d\t%s\t%d\t%s\t%9.8g\t%9.8g\t%9.8g\n', ...
                 dataset, cls, dset, featset, lbl, param, [], result );
     end
-    
+
     fprintf('> dataset\t%s\n', dataset );
     fprintf('> featureset\t%s\n', fname{featset} );
     fprintf([ '# begin cross-validation training\n> partitions\t%d\n#\n', ...
@@ -77,7 +77,7 @@ function mlp ( dataset, featset, balance, random_seeds )
             size(S.data(1).train.pseudo,   1), ...
             size(S.data(1).cv_train_pseudo,1), ...
             size(S.data(1).cv_test_pseudo, 1));
-    
+
     % create matlab pool
     num_workers = 12;
     if matlabpool('size') == 0
@@ -94,13 +94,13 @@ function mlp ( dataset, featset, balance, random_seeds )
     fprintf('# using %d matlabpool workers\n', matlabpool('size'));
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % Training 
-    
+    % Training
+
     N = length(S.hidden_sizes);
     RS = length(S.random_seeds);
     T = S.partitions;
 
-    % results 
+    % results
     res = zeros(N,RS,T);
     % details for random partition
     rand = struct();
@@ -112,19 +112,19 @@ function mlp ( dataset, featset, balance, random_seeds )
         part_train_pseudo = S.data(rs).cv_train_pseudo;
         part_test_real    = S.data(rs).cv_test_real;
         part_test_pseudo  = S.data(rs).cv_test_pseudo;
-          
+
         % details for each iteration
         rand(rs).iter = struct();
-    
+
         for t=1:T
             % shuffle data and separate labels
             traindata = shuffle( [  train_real(    part_train_real(:,mod(t,T)+1),:); ...
                                 train_pseudo(part_train_pseudo(:,mod(t,T)+1),:)] );
             rand(rs).iter(t).train_ids  = traindata(:,68:70);
             train_lbls                  = [traindata(:,67), -traindata(:,67)];
-            rand(rs).iter(t).train_lbls = train_lbls;          
+            rand(rs).iter(t).train_lbls = train_lbls;
             traindata                   = traindata(:,1:66);
-          
+
             test_real   =   train_real(  part_test_real(:,mod(t,T)+1),1:66);
             test_pseudo = train_pseudo(part_test_pseudo(:,mod(t,T)+1),1:66);
 
@@ -138,13 +138,13 @@ function mlp ( dataset, featset, balance, random_seeds )
                 Gm = 0;
                 try
                     net = patternnet( S.hidden_sizes(n) );
-                    
+
                     net.trainFcn = S.train_function;
                     net.trainParam.showWindow = 0;
                     %net.trainParam.show = 2000;
                     net.trainParam.time = 10;
                     net.trainParam.epochs = 2000000000000;
-                
+
                     net = init(net);
                     net = train(net, traindata(:,features)', train_lbls');
 
@@ -154,24 +154,24 @@ function mlp ( dataset, featset, balance, random_seeds )
                     Se = mean( res_r(:,1) == 1 );
                     Sp = mean( res_p(:,2) == 1 );
                     Gm = geomean( [Se Sp] );
-                      
+
                 catch e
                     fprintf('! %s / %s', e.identifier, e.message)
                 end % try
-              
+
                 % save Gm to results array
                 res(n,rs,t) = Gm;
             end % parfor n
         end % for t
     end % for rs
-    
+
     % save avg performance
     S.gm = mean(mean(res,3),2)';
-    
+
     % highlight best-performing paramsets
     S.better = [ abs(S.gm-max(S.gm)) < 4^(-1-2) ];
     S.best = [ abs(S.gm-max(S.gm)) < 0.00001 ];
-      
+
     if max(S.gm) < 0.25
         fprintf('! no convergence, sorry\n')
         fid = fopen( tabfile, 'a' );
@@ -182,23 +182,23 @@ function mlp ( dataset, featset, balance, random_seeds )
         fclose(fid)
         return
     end
-    
+
     fprintf('#\n# idx\t#hidden\t\tgeomean\n');
     fprintf(   '# ---\t-------\t\t-------\n');
     for d=find(S.better)
         fprintf('< %d\t%d\t%8.6f\n', d, S.hidden_sizes(d), S.gm(d) );
     end % for d
-    
+
     S.numcv = S.numcv + length(S.hidden_sizes);
     S.time  = round(etime(clock,S.begintime));
     fprintf( '#\n> time\t%02d:%02d\n', floor(S.time/60), mod(S.time,60))
-    
+
     % perform classification on test datasets
     bidx = find(S.best,1,'first');
 
     % write tsv-data
     fid = fopen( tabfile, 'a' );
-    writetab(fid, 0, 'train', S.hidden_sizes(bidx), S.gm(bidx))    
+    writetab(fid, 0, 'train', S.hidden_sizes(bidx), S.gm(bidx))
     if max(S.gm) < 0.6
         fprintf('! train CV rate too low, not testing\n')
         for i=1:length(S.data(1).test)
@@ -211,42 +211,30 @@ function mlp ( dataset, featset, balance, random_seeds )
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     fprintf('#\n# begin testing, H=%g\n',S.hidden_sizes(bidx));
-    S.test = zeros(length(S.data(rs).test), length(S.random_seeds), S.partitions);
+    S.test = zeros(length(S.data(rs).test), length(S.random_seeds));
 
     for rs=1:length(S.random_seeds)
-        
-        train_real        = S.data(rs).train.real;
-        train_pseudo      = S.data(rs).train.pseudo;
-        part_train_real   = S.data(rs).cv_train_real;
-        part_train_pseudo = S.data(rs).cv_train_pseudo;
-        
-        T = S.partitions;
-        for t=1:T
-            % train classifier 
-            traindata  = shuffle([train_real(    part_train_real(:,mod(t,T)+1),:); ...
-                                train_pseudo(part_train_pseudo(:,mod(t,T)+1),:)] );
-            train_lbls = [traindata(:,67) -traindata(:,67)];
-            traindata  = traindata(:,1:66);
-            
-            net = patternnet( S.hidden_sizes(bidx) );
-            net.trainFcn = S.train_function;
-            net.trainParam.showWindow = 0;
-            net.trainParam.time = 10;
-            net.trainParam.epochs = 2000000000000;    
-            net = init(net);
-            net = train(net, traindata(:,features)', train_lbls');
 
-            for i=1:length(S.data(rs).test)
-                res = sign(net(S.data(rs).test(i).data(:,features)'))';
-                cls_results = res(:,1).*(res(:,1)~=res(:,2));
-                S.test(i,rs,t) = mean( cls_results == S.data(rs).test(i).class);
-            end
-        end % for t        
+        train_real   = S.data(rs).train.real;
+        train_pseudo = S.data(rs).train.pseudo;
+        traindata    = stshuffle(rs,[train_real;train_pseudo]);
+        train_lbls   = [traindata(:,67) -traindata(:,67)];
+
+        net = patternnet( S.hidden_sizes(bidx) );
+        net.trainFcn = S.train_function;
+        net.trainParam.showWindow = 0;
+        net.trainParam.time = 10;
+        net.trainParam.epochs = 2000000000000;
+        net = init(net);
+        net = train(net, traindata(:,features)', train_lbls');
+
+        for i=1:length(S.data(rs).test)
+            res = sign(net(S.data(rs).test(i).data(:,features)'))';
+            cls_results = res(:,1).*(res(:,1)~=res(:,2));
+            S.test(i,rs) = mean( cls_results == S.data(rs).test(i).class);
+        end
     end % for rs
-         
-    % average for all partitions
-    S.test = mean(S.test,3);
-    
+
     % print and write test results
     fprintf('# \t\tdataset\t\t\tclass\tsize\tperformance\n');
     fprintf('# \t\t-------\t\t\t-----\t----\t-----------\n');
@@ -255,10 +243,10 @@ function mlp ( dataset, featset, balance, random_seeds )
         fprintf('+ %32s\t%d\t%d\t%8.6f\n',...
                 S.data(1).test(i).name, S.data(1).test(i).class, ...
                 size(S.data(1).test(i).data,1), mean(S.test(i,:)));
-        
+
         writetab(fid, S.data(1).test(i).class, S.data(1).test(i).name, ...
                  S.hidden_sizes(bidx),mean(S.test(i,:)))
     end
     fclose(fid);
-    
+
 end
