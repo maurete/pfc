dad6e970e486f25ed7cca10ab5bd2d9ab07395ef
Mauro Torrez <mauro@Kish.lan>
2015-01-03 03:44:11 -0300

    reprise 2015

--- src/gridtune.m
+++ src/gridtune.m
@@ -16,7 +16,7 @@ out.zoom = @zoom;
     [test aux] = gridinterp(test(ii,jj,:));
     test = [test & 1-aux]*1;
     mask = floor(gridinterp(mask(ii,jj,:)));
-    
+
     end
 
 out.threshold = @threshold;
@@ -33,14 +33,14 @@ out.threshold = @threshold;
     [test aux] = gridinterp(test);
     test = [test & 1-aux]*1;
     mask = floor(gridinterp(mask));
-    
+
     % mask values below threshold
     [zz idx]  = sort(values(1:numel(values(:,:,1))));
-    % TODO the following leaves at most 200 elements unmasked. 
+    % TODO the following leaves at most 200 elements unmasked.
     % should be more general.
     mask(idx(1:max(round(thr*numel(mask)),numel(mask)-limit))) = 1;
-    
-    end    
+
+    end
 
 out.bestneighbor = @bestneighbor;
     function [values, params, test, mask] = bestneighbor (values, params, test, mask, precision)
@@ -48,7 +48,7 @@ out.bestneighbor = @bestneighbor;
     if nargin < 5, precision = 0.25; end
 
     np = size(params,3);
-    
+
     % get dx, dy for each parameter
     cur_dx = zeros(1,np);
     cur_dy = zeros(1,np);
@@ -59,34 +59,34 @@ out.bestneighbor = @bestneighbor;
         cur_dx = reshape(params(1,2,:)-params(1,1,:),1,[],1);
     end
 
-    rw = ceil(max(cur_dx)/precision);
-    rh = ceil(max(cur_dy)/precision);
+    rw = ceil(max([cur_dx.*sign(cur_dx)]/2)/precision);
+    rh = ceil(max([cur_dy.*sign(cur_dy)]/2)/precision);
     nw = 2*rw+1;
     nh = 2*rh+1;
-    
+
     % find best central value
     [ii jj] = find(values(:,:,1)==max(max(values(:,:,1))),1,'first');
 
     cvalues = values(ii,jj,:);
     cparams = params(ii,jj,:);
-    
+
     values = zeros(nh, nw, size(values,3));
     values(rh+1,rw+1,:) = cvalues;
-    
+
     params = zeros(nh, nw, np);
     for i=1:np
         p = cparams(i);
         dx = cur_dx(i);
-        dy = cur_dy(i);        
-        params(:,:,i) = ones(nh,nw)*diag(linspace(p-dx,p+dx,nw));
-        if dy ~= 0    
-            params(:,:,i) = diag(linspace(p-dy,p+dy,nh))*ones(nh,nw);
+        dy = cur_dy(i);
+        params(:,:,i) = ones(nh,nw)*diag(linspace(p-dx/2,p+dx/2,nw));
+        if dy ~= 0
+            params(:,:,i) = diag(linspace(p-dy/2,p+dy/2,nh))*ones(nh,nw);
         end
     end
-    
+
     mask = zeros(nh, nw, size(mask,3));
     test = zeros(nh, nw, size(test,3));
-    
+
     end
 
 end
---
+++ src/optsigma.m
@@ -0,0 +1,55 @@
+function [sigma inverse] = optsigma ( data, labels, tabfile, dataset, featset )
+
+    if nargin < 5 featset = 0; end
+    if nargin < 4 dataset = '?'; end
+    if nargin < 3 tabfile = 'resultsigma'; end
+    if nargin < 2
+        labels = data(:,67);
+        data = data(:,1:66);
+    end
+
+    com = common;
+    
+    idxpos = find(labels == 1);
+    idxneg = find(labels < 1);
+        
+    sigmas = [0.001:0.001:0.009, 0.01:0.01:0.1, 0.15:0.05:0.5, 0.6:0.1:2, 2.5:0.5:10, 11:20, 25:5:50, 60:10:100, 200:50:500]; % 2.^[7:-0.5:-4];
+                                                                                                                              %sigmas = logspace(-10,10, 100);
+    result = zeros(size(sigmas));
+    resul2 = zeros(size(sigmas));
+    resnum = zeros(size(sigmas));
+    resden = zeros(size(sigmas));
+    
+    pos = data(idxpos,:);
+    neg = data(idxneg,:);
+    
+    N1 = numel(idxpos);
+    N2 = numel(idxneg);
+    
+    com.init_matlabpool();
+    
+    parfor s = 1:length(sigmas)
+        sig = sigmas(s);
+        num = ((N1 - dblsum(sig, pos, pos)/N1) + ...
+               (N2 - dblsum(sig, neg, neg)/N2));
+        den = ( dblsum(sig, pos, pos) / (N1^2) + ...
+                dblsum(sig, neg, neg) / (N2^2) - ...
+                2*dblsum(sig, pos, neg) / (N1*N2));
+        result(s) =  num/den;
+        resul2(s) =  den/num;
+        resnum(s) = num;
+        resden(s) = den;
+    end    
+    
+    plot( log2(sigmas), result/max(abs(result)), 'b', ...
+              log2(sigmas), resul2/max(abs(resul2)), 'r' ) %, ...
+                                    %sigmas, resnum/max(abs(resnum)), 'c', ...
+                                    %sigmas, resden/max(abs(resden)), 'm')
+    
+    sigma   = log2(sigmas( find(result==min(result), 1, 'first') ));
+    inverse = log2(sigmas( find(resul2==max(resul2), 1, 'first') ));
+
+    com.write_train_info(tabfile, dataset, featset, 'sigma-optim', ...
+                         sigma, inverse, -1);
+    
+end
