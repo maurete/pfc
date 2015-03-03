9f7ab03031c1caced2e6768d88e454f9b9eb01c6
Mauro Torrez <mauro@qom>
2015-03-03 20:04:38 -0300

    empirical error model selection

---
+++ src/error_empirical_csvm.m
@@ -0,0 +1,43 @@
+function [err, deriv] = error_empirical_csvm( fsvmtrain, fsvmcls, fsvmderiv, theta, part, input, target)
+
+    % number of partitions
+    npart = size(part.train, 2);
+
+    % number of svm args
+    nargs = length(theta);
+
+    % in z(i) we save SVM output for ith input vector
+    z = zeros(size(target));
+    dz_dtheta = zeros(length(target), nargs);
+
+    % train svm and predict validation data
+    for p=1:npart
+        trainset = input( part.train(:,p), : );
+        trainlbl = target( part.train(:,p) );
+        svmmodel = fsvmtrain( trainset, trainlbl, theta);
+
+        validx = part.validation(:,p);
+        % horrible fix for elements that escaped partitioning
+        if p==npart, validx = unique( [validx; find(z==0)], 'stable' ); end
+
+        validationset = input( validx, :);
+        if isa(fsvmderiv, 'function_handle')
+            z(validx) = fsvmcls(svmmodel, validationset);
+            dz_dtheta(validx,:) = fsvmderiv(svmmodel, validationset);
+        else, [z(validx), dz_dtheta(validx,:)] = fsvmcls(svmmodel, validationset);
+        end
+    end
+
+    if all(dz_dtheta(:,1)==0),
+        warning('SVM output deriv w.r.t. C is zero for all inputs.')
+    end
+
+    % train sigmoid on validation data
+    sigmoid_params = model_sigmoid_train(z, target);
+
+    [Ei dEi_dz] = error_empirical(sigmoid_params, z, target);
+    
+    err = sum(Ei);
+    deriv = dEi_dz' * dz_dtheta;
+    
+end
