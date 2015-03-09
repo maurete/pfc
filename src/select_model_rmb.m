a7c6fb144660e5a464eb0332998de12faabe8d5e
Mauro Torrez <mauro@qom>
2015-03-09 02:02:56 -0300

    select_model_empirical and select_model_rmb now expect load_problem output as first argument

--- src/select_model_rmb.m
+++ src/select_model_rmb.m
@@ -1,63 +1,32 @@
-function [svm_params, paramh, errh] = select_model_rmb ( dataset, featset, kernel, lib, ...
-                                                      theta0, max_iter, randseed, data, ...
-                                                      gtol, Delta, exponential )
-    
-    if nargin < 11|| isempty(exponential), exponential = true; end
-    if nargin < 10|| isempty(Delta),    Delta    = 1; end
-    if nargin < 9 || isempty(gtol),     gtol     = 2e-6; end
-    if nargin < 8 || isempty(data),     data     = false; end
-    if nargin < 7 || isempty(randseed), randseed = 1135; end
-    if nargin < 6 || isempty(max_iter), max_iter = 400; end
+function [svm_params, paramh, errh] = select_model_rmb ( ...
+    problem, kernel, lib, theta0, randseed, gtol, max_iter, Delta )
 
     com = common;
+    kernel = com.get_kernel(kernel);
+    svm_tol = 1e-6;
 
-    % find out if rbf kernel is selected
-    % find out if which kernel is selected
-    if     strncmpi(kernel,'rbf_uni',7);    kernel = 'rbf_uni';
-    elseif strncmpi(kernel,'rbf_unc',7);    kernel = 'rbf_unc';
-    elseif strncmpi(kernel,'linear_uni',10); kernel = 'linear_uni';
-    elseif strncmpi(kernel,'linear_unc',10); kernel = 'linear_unc';
-    elseif strncmpi(kernel,'lin',3);        kernel = 'linear';
-    elseif strncmpi(kernel,'rbf',4);        kernel = 'rbf';
-    else error('! fatal error: unknown kernel function specified.');
-    end
-
-    features = com.featindex{featset};
-
-    %%% Load data %%%
-
-    if ~isstruct(data)
-        data = struct();
-        [data.train data.test] = load_data(dataset, randseed, false);
-    end
-    trainset = com.stshuffle(randseed,[data.train.real; data.train.pseudo]);
-    trainlbl = trainset(:,67);
-    trainset = trainset(:,features);
-
-    trainfunc = @(input,target,theta) mysvm_train(lib, kernel, input, target, ...
-                                                  theta(1), theta(2:end), false, 1e-6 );
+    if nargin < 8 || isempty(Delta),    Delta    = 1; end
+    if nargin < 7 || isempty(max_iter), max_iter = 100; end
+    if nargin < 6 || isempty(gtol),     gtol     = 1e-3; end
+    if nargin < 5 || isempty(randseed), randseed = 1135; end
 
     % initial parameter vector
-    if nargin > 4 && ~isempty(theta0), theta = theta0;
-    elseif strfind(kernel,'rbf') theta = [0 0];
+    if nargin > 3 && ~isempty(theta0), theta = theta0;
+    elseif com.get_kernel(kernel,'rbf',false), theta = [0 0];
     else theta = 0;
     end
 
-    % param history to watch for convergence
-    theta_hist = theta;
-    gradient_hist = [];
-    err_hist = [];
+    trainfunc = @(input,target,theta) mysvm_train( ...
+        lib, kernel, input, target, theta(1), theta(2:end), false, svm_tol );
 
-    if exponential, tf = @exp; else tf = @(t) t; end
+    rmb_func = @(theta) error_rmb_csvm( ...
+        trainfunc, exp(theta), Delta, true, problem.trainset, problem.trainlabels );
 
-    rmb_func = @(theta) error_rmb_csvm(trainfunc, tf(theta), Delta, ...
-                                       exponential, trainset, trainlbl);
-
-    [svm_params,~,paramh,errh] = opt_bfgs_simple( rmb_func, false, theta, gtol, 100 )
+    [svm_params,~,paramh,errh] = opt_bfgs_simple( rmb_func, false, theta, gtol, max_iter )
 
     %%% test best-performing parameters %%%
 
-    res = com.run_tests(data,featset,randseed,kernel,lib,tf(svm_params(1)),tf(svm_params(2:end)) );
+    res = com.test_csvm(problem,kernel,lib,exp(svm_params(1)),exp(svm_params(2:end)),svm_tol);
     com.print_test_info(res);
 
 end
