352b3bcb56a3a2129965cc090d5ff8b19425a825
Mauro Torrez <mauro@qom>
2015-05-12 23:00:41 -0300

    load_data: add help, comment code, support optional params

--- src/loadset.m
+++ src/loadset.m
@@ -1,93 +1,85 @@
-function [out,all_sp] = loadset( name, species, id )
-
-%% Loadset - Loads named dataset by species
-
-% input:
-%     name: dataset name (see valid_names below)
-%      spc: species to load. possible values:
-%            'all' - loads all samples
-%            'human' - loads human samples only
-%            'non-numan' - non-human samples only
-%            { 'sp1', 'sp2', ... } - select species
-%                by listing them in a cell array
-%       id: identifier to be added as last column,
-%           to be used for sample tracing
+function [out,all_sp] = loadset(name,species,id)
+%LOADSET Load existing dataset with pre-computed features
 %
-% output matrix with 70 columns:
-%     1-66: feature vector:
-%             1-32 - triplet features (.3)
-%            33-36 - triplet-extra features (.3x)
-%            37-59 - sequence features (.s)
-%            60-66 - folding features (.f)
-%       67: entry class (1, 0 or -1)
-%       68: index in species file (== line no.)
-%       69: index of species file in dataset (1 to M, in
-%           alphabetical order => 'aaa'=1, 'zzz'=M)
-%       70: dataset id (same as input id)
+%   OUT = LOADSET(NAME,SPECIES,ID) loads dataset NAME to be found
+%     in ../data/ directory, for selectad SPECIES.
+%     NAME is the dataset directory name, relative to ../data/.
+%     SPECIES can be either 'all', 'human', 'non-human', or a cell
+%     array of strings for each individual species.
+%     OUT is a numeric matrix in which rows are entries, columns
+%     1-66 being features, column 67 the entry class, column 68
+%     the entry number (within respective species), column 69 the
+%     species file number (in ascending order within dataset), and
+%     column 70 the ID argument, or zero.
+%
+%   [OUT,NAMES] = LOADSET(...)
+%     Optional second output argument is a cell array of strings
+%     with the names of species files (excluding extension) present
+%     in the dataset, with indexes matching the 69th column of OUT.
+%
+%   See also PROBLEM_GEN
 
-    % valid dataset names
-    valid_names = {            'coding'; 'conserved-hairpin';            ...
-                            'mirbase50'; 'cross-species'; 'updated';     ...
-                     'mirbase82-mipred'; 'mirbase82-mipred/multi';       ...
-                     'functional-ncrna'; 'functional-ncrna/multi';       ...
-                            'mirbase12'; 'mirbase12/multi';              ...
-                  'mirbase12-micropred'; 'mirbase12-micropred/multi';    ...
-                          'other-ncrna'; 'other-ncrna/multi';            ...
-                            'mirbase20'; 'mirbase20-nr';                 ...
-                      'mirbase20/multi'; 'mirbase20-nr/multi';           ...
-                 'mirbase50/3svm-train'; 'mirbase50/3svm-test';          ...
-                    'coding/3svm-train'; 'coding/3svm-test'                };
+    if nargin < 3 || isempty(id),      id=0;          end
+    if nargin < 2 || isempty(species), species='all'; end
 
-    % validate dataset name
-    val = strcmp(valid_names, name);
-    assert(sum([val]) == 1, 'ERROR: invalid database name')
+    % Validate dataset name
+    if ~isdir(['../data/',name])
+        error('loadset: invalid database name.')
+    end
 
-    % get available species in dataset
+    % List all species in dataset
     bpath = ['../data/' name '/'];
+    % List every .c file
     d = dir( [bpath '*.c'] );
     sp = { d.name }';
-    % strip the '.c' extension from species
+    % Strip .c extension to get all species in dataset
     all_sp = cellfun(@(in)in(1:end-2),sp,'UniformOutput',false);
 
-    % find which species will be returned
+    % Select species
     sel_sp = zeros(size(all_sp));
-
-    % generate 'selected species' vector
     if isstr(species)
+        % Species argument is a string
         if strcmpi(species,'all')
+            % Match all species
             sel_sp = ones(size(all_sp));
         elseif strcmpi(species,'human')
+            % Match 'hsa' species file
             sel_sp = strcmpi(all_sp,'hsa');
         elseif strcmpi(species,'non-human')
+            % Match all but 'hsa' species
             sel_sp = 1-strcmpi(all_sp,'hsa');
         end
     elseif iscell(species)
+        % Species argument is a cell string
         for k=1:length(species)
+            % Match respective species
             sel_sp = sel_sp + strcmpi(all_sp,species(k));
+            % TODO fail on non-existent species?
         end
     end
 
-    % output array
+    % Output array
     out = [];
 
-    % loop for all available species in dataset
+    % Loop for all available species in dataset
     for j=1:length(all_sp)
 
-        % if current species was selected
+        % If current species was selected
         if sel_sp(j)
-            feat3 = dlmread( [bpath all_sp{j} '.3'], '\t' );
-            featx = dlmread( [bpath all_sp{j} '.3x'], '\t' );
-            feats = dlmread( [bpath all_sp{j} '.s'], '\t' );
-            featf = dlmread( [bpath all_sp{j} '.f'], '\t' );
-            class = dlmread( [bpath all_sp{j} '.c'], '\t' );
+            % Read files
+            feat3 = dlmread([bpath,all_sp{j},'.3'], '\t');
+            featx = dlmread([bpath,all_sp{j},'.3x'],'\t');
+            feats = dlmread([bpath,all_sp{j},'.s'], '\t');
+            featf = dlmread([bpath,all_sp{j},'.f'], '\t');
+            class = dlmread([bpath,all_sp{j},'.c'], '\t');
 
+            % Append to output matrix
             out = [ out;                        ... % current content
                     feat3, featx, feats, featf, ... % feature vector
                     class,                      ... % class
                     [1:length(class)]',         ... % entry number
                     j*ones(size(class)),        ... % species number
                     id*ones(size(class))         ]; % identifier
-
         end
     end
 end
